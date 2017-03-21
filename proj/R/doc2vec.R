5b4eff8e8bd974e6946ad949840397295691f01d
unknown <austinc6@vt.edu>
2017-03-21 19:49:25 -0400

    Created base class structure for doc2vec

--- proj/R/doc2vec.R
+++ proj/R/doc2vec.R
@@ -1,22 +1,52 @@
 # doc2vec.R
+# Currently in as basic a state as can be achieved based on the
+# Gensim doc2vec model
 
-Doc2Vec <- function(documents=NULL, dm_mean=NULL, dm=1, dbow_words=0,
-                    dm_concat=0, dm_tag_count=1, docvecs=NULL,
-                    docvecs_mapfile=NULL, comment=NULL, trim_rule=NULL)
-{
-  thisEnv <- environment()
+Doc2Vec <- setClass(
+  "Doc2Vec",
   
-  me <- list(
-    thisEnv = thisEnv,
-    
-    getEnv = function()
-    {
-      return(get("thisEnv",thisEnv))
-    }
-  )
+  slots=c(
+    # List of documents which will be used for training
+    documents="list",
+    # If 0, use sum of context word vectors. If 1, use mean.
+    dm_mean="numeric",
+    # The training algorithm. 0 for DBOW, 1 for DM
+    dm="numeric",
+    # If 0 train doc vectors only (faster), if 1 trains vectors
+    # simultaneously with DBOW
+    dbow_words="numeric",
+    # If 0, do nothing. If 1, concatenate context vectors rather 
+    # than use mean
+    dm_concat="numeric",
+    # Expected number of tags per document when dm_concat is active
+    dm_tag_count="numeric",
+    docvecs="list",
+    docvecs_mapfile="character",
+    comment="character",
+    trim_rule="function"
+  ),
   
-  assign('this', me, envir=thisEnv)
+  prototype = list(
+    # Model is left uninitialized
+    documents=NULL,
+    # Use sum by default
+    dm_mean=0,
+    # Use DM by default
+    dm=1,
+    # Train doc vectors only
+    dbow_words=0,
+    # No concatenation by default
+    dm_concat=0,
+    # 1 as a default, though only active when dm_concat is
+    dm_tag_count=1,
+    docvecs=NULL,
+    docvecs_mapfile=NULL,
+    comment=NULL,
+    # Default trim rule, defined in word2vec.R
+    trim_rule=DefaultTrimRule
+  ),
   
-  class(me) <- append(class(me), "Doc2Vec")
-  return(me)
-}
+  validity = function() {
+    # Do something
+  }
+)
